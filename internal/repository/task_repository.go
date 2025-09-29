1d2b750e3b7c6ed2354b940f332e8ad62c31ee3e
s.prokopchuk <s.prokopchuk@ddemo.ru>
2025-09-29 14:37:13 +0300

    Добавил оставшиеся ручки

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -2,13 +2,13 @@ package repository
 
 import (
 	"todo/internal/models"
+	"todo/internal/requests"
 
 	"gorm.io/gorm"
 )
 
 type TaskRepository struct {
 	db    *gorm.DB
-	Task  models.Task
 	Tasks []models.Task
 }
 
@@ -22,7 +22,40 @@ func (tr *TaskRepository) GetAll() ([]models.Task, error) {
 	return tr.Tasks, r.Error
 }
 
-func (tr *TaskRepository) GetByID(id string) (models.Task, error) {
-	r := tr.db.First(&tr.Task, id)
-	return tr.Task, r.Error
+func (tr *TaskRepository) GetByID(id int) (models.Task, error) {
+	var task models.Task
+	r := tr.db.First(&task, id)
+	return task, r.Error
+}
+
+func (tr *TaskRepository) Create(task models.Task) (models.Task, error) {
+	r := tr.db.Create(&task)
+
+	return task, r.Error
+}
+
+func (tr *TaskRepository) Update(id int, task requests.TaskRequest) (models.Task, error) {
+	ut, err := tr.GetByID(id)
+	if err != nil {
+		return models.Task{}, err
+	}
+
+	ut.Title = task.Title
+	ut.Description = task.Description
+	ut.Completed = task.Completed
+	ut.DateTimeTask = task.DateTimeTask
+	r := tr.db.Save(&ut)
+
+	return ut, r.Error
+}
+
+func (tr *TaskRepository) Delete(id int) error {
+	dt, err := tr.GetByID(id)
+	if err != nil {
+		return err
+	}
+
+	r := tr.db.Delete(&dt, id)
+
+	return r.Error
 }
